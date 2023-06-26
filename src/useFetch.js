632adcfcc73f5c94cf5dccfd565d86aa653f8ac1
Shoxa <bapeshoxa@gmail.com>
2023-06-26 10:50:43 +0500

    Update useFetch.js

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,10 +1,46 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 
-function useFetch() {
+function useFetch(url) {
 
-    
+    const [data, setData] = useState(null);  
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null)
 
+    useEffect(() => {
+        setLoading(true)
+        axios
+        .get(url)
+        .then((response) => {
+            setData(response.data)
+        })
+        .catch((err) => {
+            setError(err)
+        })
+        .finally(() => {
+            setLoading(false)
+        })
+    }, [url])
+
+
+    const request = () => {
+        setLoading(true)
+        axios
+        .get(url)
+        .then((response) => {
+            setData(response.data)
+        })
+        .catch((err) => {
+            setError(err)
+        })
+        .finally(() => {
+            setLoading(false)
+        })
+    }
+
+    return {error, loading, data, request}
 
 }
+
+export default useFetch;
